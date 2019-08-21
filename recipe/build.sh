78033d68146d4f1537304e8f6b6cf6eec12e5c35
buijennifer <bui.jennifert@gmail.com>
2019-08-21 20:36:47 +0000

    fix install line

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,4 +4,4 @@ set -ex
 export CFLAGS="${CFLAGS} -I${PREFIX}/include"
 export LDFLAGS="${LDFLAGS} -I${PREFIX}/lib"
 
-{{ PYTHON }} -m pip install . --no-deps -v
+python -m pip install . --no-deps -v
